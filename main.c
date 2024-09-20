248b30a8ff63201105acabce8799933600c60a19
Jakub Oleksiak <yakubofficialmail@gmail.com>
2024-09-20 09:16:36 +0200

    GLFW Template
    Written a template for OpenGL programming using GLFW

--- main.c
+++ main.c
@@ -1,51 +1,42 @@
-#include "SDL2/SDL.h"
 #include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
 int main(int argc, const char* argv[]) {
-	if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+	if(!glfwInit())
+		return 1;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(
+		800, 
+		600, 
+		"Hello, OpenGL!", 
+		NULL, 
+		NULL
+	);
+
+	if(!window) {
+		glfwTerminate();
 		return 1;
 	}
 
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-
-	SDL_Window* window = SDL_CreateWindow(
-		"Hello, OpenGL!",
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		800,
-		600,
-		SDL_WINDOW_OPENGL
-	);
-
-	SDL_GLContext context = SDL_GL_CreateContext(window);
-	SDL_GL_MakeCurrent(window, context);
-	SDL_GL_SetSwapInterval(1);
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 
 	gladLoadGL();
 
-	int exit = 0;
-	while(!exit) {
-		SDL_Event event = { 0 };
-		while(SDL_PollEvent(&event)) {
-			switch(event.type) {
-				case SDL_QUIT: {
-					exit = 1;
-				} break;
-			}
-		}
-
+	while(!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
-		SDL_GL_SwapWindow(window);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
-	SDL_GL_DeleteContext(context);
-	SDL_DestroyWindow(window);
-
-	SDL_Quit();
+	glfwDestroyWindow(window);
+	glfwTerminate();
 
 	return 0;
 }
